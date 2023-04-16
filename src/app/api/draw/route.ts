82d075cdf610e38a91ca31b60a0c0aa798a8e4bb
chetan baliyan <chetanbaliyan10@gmail.com>
2023-04-16 13:28:06 +0530

    fix api

--- src/app/api/draw/route.ts
+++ src/app/api/draw/route.ts
@@ -1,6 +1,6 @@
 import { log } from "console";
 
-const VibePrompts = {
+const VibePrompts: any = {
   "ultra-realistic":
     "ultra realistic, best quality, real world, high resolution, extremely detailed, 4k",
   "low-poly": "low poly, low polygon, low poly art, low poly style",
@@ -12,7 +12,7 @@ const VibePrompts = {
     "olpntng style, girl,cute,real, oil painting, heavy strokes, paint dripping",
 };
 
-const VibeDescriptionPrefixes = {
+const VibeDescriptionPrefixes: any = {
   "ultra-realistic": "A photo of a ",
   "low-poly": "A low-poly rendering of a ",
   anime: "An anime-style rendering of a ",
@@ -20,29 +20,12 @@ const VibeDescriptionPrefixes = {
   painting: "A painting of a ",
 };
 
-export async function POST(request, response) {
-  // const { vibe, description, imageUrl } = await request.json();
-  const vibe = "ultra-realistic";
-  const description = "cat";
+export async function POST(request: Request) {
+  const { vibe, description, imageUrl } = await request.json();
 
-  let imageUrl =
-    "https://upcdn.io/kW15bDc/raw/uploads/2023/04/15/file-4v9x.jpeg";
-
-  log(
-    JSON.stringify({
-      version:
-        "435061a1b5a4c1e26740464bf786efdfa9cb3a3ac488595a2de23e143fdb0117",
-      inputs: {
-        image: imageUrl,
-        prompt: `${VibeDescriptionPrefixes[vibe]}${description}}`,
-        ddim_steps: 30,
-        scale: 15,
-        a_prompt: VibePrompts[vibe],
-        n_prompt:
-          "longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality",
-      },
-    })
-  );
+  if (vibe || description || imageUrl) {
+    throw new Error("Missing required fields");
+  }
 
   let res = await fetch("https://api.replicate.com/v1/predictions", {
     method: "POST",
@@ -53,9 +36,9 @@ export async function POST(request, response) {
     body: JSON.stringify({
       version:
         "435061a1b5a4c1e26740464bf786efdfa9cb3a3ac488595a2de23e143fdb0117",
-      inputs: {
+      input: {
         image: imageUrl,
-        prompt: `${VibeDescriptionPrefixes[vibe]}${description}}`,
+        prompt: `${VibeDescriptionPrefixes[vibe]}${description}`,
         ddim_steps: 30,
         scale: 15,
         a_prompt: VibePrompts[vibe],
@@ -67,11 +50,9 @@ export async function POST(request, response) {
 
   let resJson = await res.json();
 
-  log(resJson);
-
   let pollUrl = resJson.urls.get;
 
-  let originalImageUrl = resJson.inputs.image;
+  let originalImageUrl = resJson.input.image;
 
   let id = resJson.id;
 
@@ -87,19 +68,28 @@ export async function POST(request, response) {
     });
 
     let jsonResponseDone = await completedRes.json();
-
+    log(jsonResponseDone.status);
     if (jsonResponseDone.status === "succeeded") {
-      genImageUrl = jsonResponseDone.outputs[1] as string;
+      genImageUrl = jsonResponseDone.output[1] as string;
     } else if (jsonResponseDone.status === "failed") {
+      throw new Error("Failed to generate image");
       break;
     } else {
       await new Promise((resolve) => setTimeout(resolve, 1000));
     }
   }
 
-  return response.ststus(200).json({
-    orignal: originalImageUrl,
-    generated: genImageUrl,
-    id: id,
-  });
+  return new Response(
+    JSON.stringify({
+      orignal: originalImageUrl,
+      generated: genImageUrl,
+      id: id,
+    })
+  );
+
+  // return response.json({
+  //   orignal: originalImageUrl,
+  //   generated: genImageUrl,
+  //   id: id,
+  // });
 }
