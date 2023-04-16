b80f2f28741a960eaaedd14a47e70aadc2b5b9e4
chetan baliyan <chetanbaliyan10@gmail.com>
2023-04-17 01:40:46 +0530

    feat: change metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Draw AI - Transform Your Drawing Ideas into Reality</title>
+        <link rel="icon" href="/logo.png" />
+        <meta
+          name="description"
+          content="Draw ai app to convert your drawing ideas into reality."
+          key="desc"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
